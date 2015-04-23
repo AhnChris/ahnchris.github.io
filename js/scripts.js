e1ff183b8368b3dda8e093679d9eb4d864084d5b
Christopher Ahn <ahn.chris1@gmail.com>
2015-04-23 06:46:16 -0700

    made navbar sticky

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
-var aboutText = "";
+var aboutText = "I'm a recent graduate of the University of California, Riverside. I completed" +
+				" my studies with a BS degree in Electrical Engineering in the Spring of 2014." +
+				" I like to build embedded systems, but recently I have discovered that I enjoy" +
+				" developing software much more, mainly mobile apps. I also enjoy problem solving" +
+				" and learning new technologies.";
 document.getElementById("about").innerHTML = aboutText;
+
+
+
+var header = document.querySelector('.navbar');
+var origOffsetY = header.offsetTop;
+
+function onScroll(e) {
+  window.scrollY >= origOffsetY ? header.classList.add('sticky') :
+                                  header.classList.remove('sticky');
+}
+
+document.addEventListener('scroll', onScroll);
