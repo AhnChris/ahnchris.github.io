e7a7c622bf89918d97f04fd09b97242477d58ab1
Christopher Ahn <ahn.chris1@gmail.com>
2016-03-20 23:29:54 -0700

    update icons and try out angular

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
-var aboutText = "I'm a recent graduate of the University of California, Riverside. I completed" +
-				" my studies with a BS degree in Electrical Engineering in the Spring of 2014." +
-				" I like to build embedded systems, but recently I have discovered that I enjoy" +
-				" developing software much more, mainly mobile apps. I also enjoy problem solving" +
-				" and learning new technologies.";
-document.getElementById("aboutBlock").innerHTML = aboutText;
+var aboutApp = angular.module("app", []);
+aboutApp.controller("aboutCtrl", function($scope) {
+	$scope.aboutBlock = "Hi, my name is Chris and I am a graduate of the University of California, Riverside. I completed my studies with a" +
+						" BS degree in Electrical Engineering in the Spring of 2014 and have since pursued a career in Software Development." +
+						" I decided to follow through with this path because I found it amazing how much impact making quality software products" +
+						" can have toward everyday people, and I wanted to be apart of making that happen. I am interested in developing android apps" +
+						" because it is exciting to see the potential mobile apps have in creating new ways to connect and interact with our mobile devices.";
+});
+
+aboutApp.filter('textAsHtml', function($sce) {
+	return function(val) {
+		return $sce.trustAsHtml(val);
+	};
+});
 
 
 
